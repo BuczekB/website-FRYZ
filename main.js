cb0af596cc84bc27601326f56c8234210c354fcf
Jakuba Buczyński <72788108+BuczekB@users.noreply.github.com>
2021-10-11 11:50:26 +0200

    add rwd

--- main.js
+++ main.js
@@ -7,27 +7,77 @@ const singleImg = document.querySelector('.single-img');
 const bodyPage = document.querySelector('body');
 
 
+const firstBox = document.querySelector('.first');
+const secoundBox = document.querySelector('.secound');
+const cyrcle = document.querySelector('.cyrcleSecound');
+const cyrcleFirst = document.querySelector('.cyrcleFirst');
+const boxListFirst = document.querySelector('.box-list-first');
+const boxListSecound = document.querySelector('.box-list-secound');
+const sf = document.querySelector('.sf');
+const sk = document.querySelector('.sk');
+
+const priceListBox = document.querySelector('.full-bg-priceList')
 
 
-const gallerySizeImg = (e) => {
+const changeToFullFirst = () => {
+    firstBox.classList.remove('first');
+    firstBox.classList.add('firstFull');
+    secoundBox.classList.add('secoundMin');
+    secoundBox.classList.remove('secound');
+    cyrcleFirst.style.display = 'block';
+    boxListFirst.style.display = 'flex';
+    sf.style.display = 'none';
+    sk.style.display = 'none';
 
+}
+const changeToFullSecound = () => {
+    secoundBox.classList.add('secoundFull');
+    secoundBox.classList.remove('secound');
+    firstBox.classList.add('firstMin');
+    firstBox.classList.remove('first');
+    cyrcle.style.display = 'block';
+    boxListSecound.style.display = 'flex';
+    sk.style.display = 'none'; 
+    sf.style.display = 'none';
 
-    const imgBox = e.target;
-    const imgAttribute = imgBox.getAttribute('data-original');
-
-    galleryFullImg.src = imgAttribute;
-
-
-
-    galleryFullImgBox.style.display = 'block';
 }
 
-gallery.addEventListener('click', gallerySizeImg);
+const changeCircleFirst = () => {
+    boxListFirst.style.display = 'none';
+    cyrcleFirst.style.display = 'none';
+    sf.style.display = 'block';
+    sk.style.display = 'block';
+    firstBox.classList.remove('firstFull');
+    firstBox.classList.add('first');
 
-const visibleFullImg = () => {
-    galleryFullImgBox.style.display = 'none';
+    secoundBox.classList.add('secound');
+    secoundBox.classList.remove('secoundMin');
+    console.log('workFirst');
+
 
 }
+const changeCircleSecound = () => {
+    sf.style.display = 'block';
+    sk.style.display = 'block';
+    boxListSecound.style.display = 'none';
+    cyrcle.style.display = 'none';
+    secoundBox.classList.add('secound');
+    secoundBox.classList.remove('secoundFull');
+    firstBox.classList.add('first');
+    firstBox.classList.remove('firstMin');
+    console.log('workSecound');
+
+
+}
+
+cyrcle.addEventListener('click', changeCircleSecound)
+cyrcleFirst.addEventListener('click', changeCircleFirst)
+sk.addEventListener('click', changeToFullFirst)
+sf.addEventListener('click', changeToFullSecound)
+
+
+
+
 
 exitSquare.addEventListener('click', visibleFullImg);
 
